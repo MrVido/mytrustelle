3d10b1ec41a09cad0f6365a4b8b811f104779d1b
MrVido <zvonividovic@gmail.com>
2024-03-07 18:57:41 -0500

    filestructure update

---
+++ marketfrontend/src/components/product/ProductCarousel.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+import { Product } from '../../../../marketplace-frontend/types';
+
+interface ProductCarouselProps {
+  products: Product[]; // Use the Product interface from ProductCard.tsx
+}
+
+const ProductCarousel: React.FC<ProductCarouselProps> = ({ products }) => {
+  return (
+    <div className="product-carousel">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductCarousel;
